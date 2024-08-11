6043c9adb2007cd50a06e894d0a5a46c9bbb9c80
ko-dev <khayr0awndev@gmail.com>
2024-08-11 20:57:36 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -22,6 +22,7 @@ Route::post('/productsSearch', [ProductsController::class, 'proSearch']);
 Route::post('/product/{slug}', [ProductsController::class, 'product'])->name('product');
 Route::post('projectDetails', [ProductsController::class, 'projectDetails']);
 
+Route::post('register', [CustomerController::class, 'register'])->name('register');
 Route::post('login', [CustomerController::class, 'login'])->name('login');
 Route::post('addNewCustomer', [CustomerController::class, 'addNewCustomer'])->name('addNewCustomer');
 Route::post('placeOrder', [CustomerController::class, 'placeOrder'])->name('placeOrder');
