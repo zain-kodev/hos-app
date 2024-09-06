72f576d5b886e69fee9f3d67a3fb85a855dd6e48
ko-dev <khayr0awndev@gmail.com>
2024-09-06 23:56:53 +0300

    uer56

--- config/filesystems.php
+++ config/filesystems.php
@@ -35,7 +35,10 @@ return [
             'root' => storage_path('app'),
             'throw' => false,
         ],
-
+        'public_uploads' => [
+            'driver' => 'local',
+            'root'   => public_path() . '/uploads',
+        ],
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
--- resources/views/AdminDashboard/online_store/products/product_edit.blade.php
+++ resources/views/AdminDashboard/online_store/products/product_edit.blade.php
@@ -102,7 +102,7 @@
                                     </div>
                                 </div>
                                 <div class="col-lg-6 col-xl-6 pe-lg-2 mb-3">
-                                    <img class="dz-preview-img" src="{{ asset($product->proImg) }}" />
+                                    <img class="dz-preview-img" src="{{ asset($product->proImg) }}" style="max-height: 350px !important;" />
                                 </div>
 
                             </div>
